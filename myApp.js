aa884ec991d9b977c4b254b0ec4e78b075466286
Harvey Zhao <86583647+harveyzzhao@users.noreply.github.com>
2023-07-02 11:36:09 +0800

    added root GET method and testing file

--- myApp.js
+++ myApp.js
@@ -1,37 +1,10 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
 
 console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
